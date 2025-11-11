043320e85f511975fa8741e1cca9e4be5dad3dc4
federicosimone <federico.simone15@gmail.com>
2025-11-11 23:12:47 +0100

    preventDefault

---
+++ main.js
@@ -0,0 +1,8 @@
+const chilomUt = document.querySelector('#km');
+const age = document.querySelector('#age') ;
+const button = document.querySelector('#button');
+
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+}) ;
+
