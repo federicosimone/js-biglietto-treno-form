e17a31a3e62e01c9f9b3a1b691aa7420566b576f
federicosimone <federico.simone15@gmail.com>
2025-11-11 23:41:11 +0100

    program ok

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-const chilomUt = document.querySelector('#km');
-const age = document.querySelector('#age') ;
+
 const button = document.querySelector('#button');
+const chilomUt = document.querySelector('#km');
+const age = document.querySelector('#age');
 
 button.addEventListener("click", function(event) {
     event.preventDefault();
 }) ;
+
+button.addEventListener("click", function(){
+    let finalPrice = null ;
+    let km = parseInt(chilomUt.value) ;
+    const eta = parseInt(age.value);
+
+
+    let price = km * 0.21 ;
+    //debugger
+    if (eta < 18) {
+        finalPrice = price - (price / 100) *20 ;
+    } else if (eta >= 18 && eta < 65 ){
+        finalPrice = price ;   
+    } else if ( eta >= 65) {
+        finalPrice = price - (price/100) * 40 ;
+    }
+    console.log(finalPrice)
+})
 
